122bc11a481c55f2845aebdf79bb7f9b2f380441
Swarnavo2003 <swarnavo2020@gmail.com>
2025-11-09 02:02:34 +0530

    feat: add dashboard ui boilerplate setup

---
+++ app/dashboard/_components/app-sidebar.tsx
@@ -0,0 +1,158 @@
+"use client"
+
+import * as React from "react"
+import {
+  AudioWaveform,
+  BadgePercent,
+  BarChart3,
+  BookOpen,
+  Boxes,
+  Building2,
+  CalendarCheck2,
+  Command,
+  CreditCard,
+  GalleryVerticalEnd,
+  LayoutDashboard,
+  PackageCheck,
+  ScissorsSquare,
+  Settings2,
+  UserCog,
+  Users,
+} from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+import { NavMain } from "./nav-main"
+import { TeamSwitcher } from "./team-switcher"
+import { NavUser } from "./nav-user"
+
+const data = {
+  user: {
+    name: "Test User",
+    email: "test@test.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  teams: [
+    {
+      name: "Acme Inc",
+      logo: GalleryVerticalEnd,
+      plan: "Enterprise",
+    },
+    {
+      name: "Acme Corp.",
+      logo: AudioWaveform,
+      plan: "Startup",
+    },
+    {
+      name: "Evil Corp.",
+      logo: Command,
+      plan: "Free",
+    },
+  ],
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "/dashboard",
+      icon: LayoutDashboard,
+      isActive: true,
+    },
+    {
+      title: "Analytics",
+      url: "/dashboard/analytics",
+      icon: BarChart3,
+      isActive: false,
+    },
+    {
+      title: "Members",
+      url: "/dashboard/members",
+      icon: Users,
+      isActive: false,
+    },
+    {
+      title: "Billing",
+      url: "/dashboard/billing",
+      icon: CreditCard,
+      isActive: false,
+    },
+    {
+      title: "Bookings",
+      url: "/dashboard/bookings",
+      icon: CalendarCheck2,
+      isActive: false,
+    },
+    {
+      title: "Inventory",
+      url: "/dashboard/inventory/products",
+      icon: Boxes,
+      items: [
+        {
+          title: "Products",
+          url: "/dashboard/inventory/products",
+        },
+        {
+          title: "Stock Register",
+          url: "/dashboard/inventory/stock-register",
+        },
+      ],
+    },
+    {
+      title: "Settings",
+      url: "/dashboard/settings",
+      icon: Settings2,
+      items: [
+        {
+          title: "Salon/Branch",
+          url: "/dashboard/settings/salon",
+          icon: Building2,
+        },
+        {
+          title: "Services",
+          url: "/dashboard/settings/services",
+          icon: ScissorsSquare, 
+        },
+        {
+          title: "Combo Plans",
+          url: "/dashboard/settings/combo-plans",
+          icon: PackageCheck,
+        },
+        {
+          title: "Subscriptions",
+          url: "/dashboard/settings/subscriptions",
+          icon: BookOpen,
+        },
+        {
+          title: "Staff",
+          url: "/dashboard/settings/staff",
+          icon: UserCog,
+        },
+        {
+          title: "Coupons",
+          url: "/dashboard/settings/coupons",
+          icon: BadgePercent,
+        },
+      ],
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,30 @@
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "./_components/app-sidebar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+export default function Dashboard() {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+          <div className="flex items-center justify-between gap-2 px-4 w-full">
+            <SidebarTrigger className="-ml-1" />
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+              <AvatarFallback>SC</AvatarFallback>
+            </Avatar>
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div className="bg-muted/50 aspect-video rounded-xl" />
+            <div className="bg-muted/50 aspect-video rounded-xl" />
+            <div className="bg-muted/50 aspect-video rounded-xl" />
+          </div>
+          <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
